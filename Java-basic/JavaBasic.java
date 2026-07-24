a7ca8e2d77bd69b1ba1dba47b6073c9d424c3712
GUDDU4631 <guddukumarchaudhary3547@gmail.com>
2026-07-24 11:13:37 +0530

    first commit

---
+++ Java-basic/JavaBasic.java
@@ -0,0 +1,12 @@
+
+class JavaBasic{
+    public static void main(String agr[]){
+        int a =10;
+        int b = 12;
+        if(a>b){
+            System.out.println(a);
+        } else{
+            System.out.println(b);
+        }
+    }
+}
