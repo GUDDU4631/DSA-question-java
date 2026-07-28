01887c2c85ab8709ed324b89c52a09b91c5728f0
GUDDU4631 <guddukumarchaudhary3547@gmail.com>
2026-07-28 21:07:26 +0530

    Day-3,4,5,6

--- Java-basic/function.java
+++ Java-basic/function.java
@@ -8,50 +8,196 @@
 //     }
 // }
 
+// ------------------------ Binomial coefficient ----------------------
 
-// Binomial coefficient 
-public class function{
-    public static int fac(int Num) {
-        int fac = 1;
-        for(int i =1 ; i<= Num; i++){
-            fac = fac * i;
+// public class function{
+//     public static int fac(int Num) {
+//         int fac = 1;
+//         for(int i =1 ; i<= Num; i++){
+//             fac = fac * i;
+//         }
+//         return fac;
+//     }
+//     public static int binCoeff(int n, int r){
+//         int a = fac(n);
+//         int b = fac(r);
+//         int c = fac(n-r);
+//         int Binomial_coefficient = a/(b*c);
+//         return Binomial_coefficient;
+//     }
+//     public static void main(String sgr[]){
+//         System.out.print(binCoeff(5,2));
+//     }
+// }
+
+
+//--------------------- PRIME NUMBER ----------------------
+
+
+// public class function {
+
+    // public static boolean isPrime(int num){
+    // boolean isPrime = true;
+    // for(int i =2; i<num ; i++){
+    // if(num%i == 0){
+    // isPrime = false;
+    // } else {
+    // isPrime = true;
+    // }
+    // }
+    // return isPrime;
+    // }
+
+    // optimize version 
+
+    // public static boolean isPrime(int num) {
+    //     if (num == 2) {
+    //         return true;
+    //     }
+    //     for (int i = 2; i <= Math.sqrt(num); i++) {
+    //         if (num % i == 0) {
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+    // public static void primeInRange(int num){
+//         for(int i=1 ; i<=num;i++){
+//             if(isPrime(i) == true){
+//                 System.out.println(i);
+//             }
+//         }
+//     }
+//     public static void main(String agr[]) {
+//         primeInRange(10);
+//     }
+// }
+
+
+
+//-------------------- Binary to Decimal ----------------------
+
+
+// public class function {
+//     public static int binaryToDecimal(int num){
+//         
+//         int decimalValue = 0;
+//         int power = 0;
+//         while(num>0){
+//            int r = num%10 ;
+//             decimalValue += r * (int)Math.pow(2,power);
+//             power++;
+//             num = num/10;
+//         }
+
+//         return decimalValue;
+//     }
+//     public static void main(String agr[]){
+//         System.out.print(binaryToDecimal(101));
+//     }
+// }
+
+
+//--------------------- Decimal to binary -----------------
+
+
+// public class function {
+//     public static int decimaltoBinary(int num){
+//         int bin=0;
+//         int power= 0;
+//         while(num > 0){
+//            int remender = num%2;
+//             bin += remender * (int)Math.pow(10,power);
+//             power++;
+//             num = num/2;
+//         }
+//         return bin;
+//     }
+//     public static void main(String agr[]){
+//         System.out.print(decimaltoBinary(5));
+//     }
+// }
+
+
+//----------------- palindrome ----------------
+
+// public class function {
+//     public static boolean isPalindrome(int num){
+//         boolean isPalindrome = false;
+//         int originalNum = num;
+//         int rev =0;
+//         while(num>0){
+//             int rem = num%10;
+//             rev = (rev * 10) + rem;
+//             num = num/10;
+//         }
+//         if(originalNum == rev){
+//             isPalindrome = true;
+//         }
+//         return isPalindrome;
+//     }
+//     public static void main(String agr[]){
+//         System.out.println(isPalindrome(152));
+//     }
+// }
+
+
+public class function {
+    public static int sumOfDigits(int num){
+        int sum = 0;
+        while(num>0){
+            int rem  = num%10;
+            sum += rem;
+            num = num/10;
         }
-        return fac;
+        return sum;
     }
-    public static int binCoeff(int n, int r){
-        int a = fac(n);
-        int b = fac(r);
-        int c = fac(n-r);
-        int Binomial_coefficient = a/(b*c);
-        return Binomial_coefficient;
-    }
-    public static void main(String sgr[]){
-        System.out.print(binCoeff(5,2));
+    public static void main(String agr[]){
+        System.out.println(sumOfDigits(152));
     }
 }
 
+
+
+
+
+
+
+
 /*
-    The function is mainly 3 types 
-        1. simple function 
-
-            return_type(){
-                your work
-                return
-            }
-        2. parameter function
-
-            return_type(int , int){
-                return int
-            }
-        3. 
+ * ------------------ The function is mainly 3 types
+ * 1. simple function
+ * 
+ * return_type(){
+ * your work
+ * return
+ * }
+ * 2. parameter function
+ * 
+ * return_type(int , int){
+ * return int
+ * }
+ * 3.
+ */
+// --------------------------------------------------------------------
+/*
+ * The value which is define while making the function is called parameter
+ * and when it called with some value that is called argument
+ */
+/*
+ * - The functions is stored in memory as CALL STACK
+ * - In call by value the actual value did not change
+ * - Call by reference change the value but in java there is no call by
+ * reference
+ */
+// ---------------------------------------------------------------------------
+/*
+ * Function mainly two type :
+ * 1. User define function
+ * 2. Inbuilt function
  */
 
 /*
-    The value which is define while making the function is called parameter 
-    and when it called with some value that is called argument
- */
-/*
-    *- The functions is stored in memory as CALL STACK
-    *- In call by value the actual value did not change 
-    *- Call by reference change the value but in java there is no call by reference
+ * -----------Function overloading---------------------
+ * multiple function in same class with same name but defferent return type..
  */
--- Java-basic/starPattern.java
+++ Java-basic/starPattern.java
@@ -1,4 +1,4 @@
-// OUTPUT 
+// OUTPUT
 // *
 // **
 // ***
@@ -11,18 +11,17 @@
 // **********
 
 // public class starPattern {
-//     public static void main(String agr[]){
-//         for(int i=0;i<10;i++){
-//             for(int j=0;j<=i;j++){
-//                 System.out.print("*");
-//             }
-//             System.out.print("\n");
-//         }
-//     }
+// public static void main(String agr[]){
+// for(int i=1;i<=10;i++){
+// for(int j=1;j<=i;j++){
+// System.out.print("*");
+// }
+// System.out.print("\n");
+// }
+// }
 // }
 
-
-// OUTPUT 
+// OUTPUT
 // ***********
 // **********
 // *********
@@ -35,104 +34,100 @@
 // **
 // *
 // public class starPattern {
-//     public static void main(String agr[]){
-//         for(int i=10;i>=0;i--){
-//             for(int j=0;j<=i;j++){
-//                 System.out.print("*");
-//             }
-//             System.out.print("\n");
-//         }
-//     }
+// public static void main(String agr[]){
+// for(int i=1;i<=10;i++){
+// for(int j=1;j<= 10 - i +1 ;j++){
+// System.out.print("*");
+// }
+// System.out.print("\n");
+// }
+// }
 // }
 
-
-// OUTPUT 
-//            *
-//           **
-//          ***
-//         ****
-//        *****
-//       ******
-//      *******
-//     ********
-//    *********
-//   **********
-//  ***********
+// OUTPUT
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+// *******
+// ********
+// *********
+// **********
+// ***********
 
 // public class starPattern {
-//     public static void main(String agr[]){
+// public static void main(String agr[]) {
 
-//         for(int i=10;i>=0;i--){
-//             for(int j=0 ; j<=i;j++){
-//                 System.out.print(" ");
-//             }
-//             for(int k = i; k<=10;k++){
-//                 System.out.print("*");
-//             }
-//             System.out.print("\n");
-//         }
-//     }
+// for (int i = 1; i <= 10; i++) {
+// for (int j = 1; j <= 10 - i; j++) {
+// System.out.print(" ");
+// }
+// for(int k =1; k<=i;k++){
+// System.out.print("*");
+// }
+// System.out.print("\n");
+// }
+// }
 // }
 
-
-// OUTPUT 
-// + + + + + + + + + + + 
-// + + + + + + + + + + + 
-// + + + + + + + + + + + 
-// + + + + + + + + + + + 
-// + + + + + + + + + + + 
-// + + + + + + + + + + + 
-// + + + + + + + + + + + 
-// + + + + + + + + + + + 
-// + + + + + + + + + + + 
-// + + + + + + + + + + + 
-// + + + + + + + + + + + 
+// OUTPUT
+// + + + + + + + + + + +
+// + + + + + + + + + + +
+// + + + + + + + + + + +
+// + + + + + + + + + + +
+// + + + + + + + + + + +
+// + + + + + + + + + + +
+// + + + + + + + + + + +
+// + + + + + + + + + + +
+// + + + + + + + + + + +
+// + + + + + + + + + + +
+// + + + + + + + + + + +
 
 // public class starPattern {
-//     public static void main(String agr[]){
+// public static void main(String agr[]){
 
-//         for(int i=0;i<=10;i++){
-//             for(int j=0 ; j<=10;j++){
-//                 System.out.print("+ ");
-//             }
-//             System.out.print("\n");
-//         }
-//     }
+// for(int i=0;i<=10;i++){
+// for(int j=0 ; j<=10;j++){
+// System.out.print("+ ");
+// }
+// System.out.print("\n");
+// }
+// }
 // }
 
 // OUTPUT
 // * * * * * * * * * * *
-// *                   *
-// *                   *
-// *                   *
-// *                   *
-// *                   *
-// *                   *
-// *                   *
-// *                   *
-// *                   *
+// * *
+// * *
+// * *
+// * *
+// * *
+// * *
+// * *
+// * *
+// * *
 // * * * * * * * * * * *
 
-
 // public class starPattern {
-//     public static void main(String agr[]) {
+// public static void main(String agr[]) {
 
-//         for (int i = 0; i <= 10; i++) {
-//                 for (int j = 0; j <= 10; j++) {
-//                     if(j==0 || j==10 || i==0 || i==10){
-//                         System.out.print("* ");
-//                     } else{
-//                         System.out.print("  ");
-//                     }
-//                 }
-            
-//             System.out.println("");
-//         }
-//     }
+// for (int i = 0; i <= 10; i++) {
+// for (int j = 0; j <= 10; j++) {
+// if(j==0 || j==10 || i==0 || i==10){
+// System.out.print("* ");
+// } else{
+// System.out.print(" ");
+// }
 // }
 
+// System.out.println("");
+// }
+// }
+// }
 
-// OUTPUT 
+// OUTPUT
 // 1
 // 01
 // 101
@@ -144,168 +139,163 @@
 // 101010101
 // 0101010101
 
-// public class starPattern {
-//     public static void main(String agr[]) {
-//         for (int i = 1; i <= 10; i++) {
-//             if(i%2==0){
-//                 for(int j =1 ; j<=i;j++){
-//                     if(j%2==0){
-//                     System.out.print("1");
-//                     } else{
-//                         System.out.print("0");
-//                     }
-//                 }
-//             } else{
-//                 for(int j =1 ; j<=i;j++){
-//                     if(j%2==0){
-//                         System.out.print("0");
-//                     } else{
-//                         System.out.print("1");
-//                     }
-//                 }
-//             }
-//             System.out.print("\n");
-//         }
-//     }
-// }
-
-
-// OUTPUT 
-// 1 
-// 2 3 
-// 4 5 6 
-// 7 8 9 10 
-// 11 12 13 14 15 
-// 16 17 18 19 20 21 
-// 22 23 24 25 26 27 28 
-// 29 30 31 32 33 34 35 36 
-// 37 38 39 40 41 42 43 44 45 
-// 46 47 48 49 50 51 52 53 54 55 
-// 56 57 58 59 60 61 62 63 64 65 66 
-
-// public class starPattern {
-//     public static void main(String agr[]) {
-//         int num = 1;
-//         for (int i = 0; i <= 10; i++) {
-//            for(int j =0; j<=i; j++){
-//             System.out.print(num+ " ");
-//             num++;
-//            }
-//             System.out.print("\n");
-//         }
-//     }
-// }
-
-
-// OUTPUT 
-//                       + + + + + + + + + + + 
-//                     + + + + + + + + + + + 
-//                   + + + + + + + + + + + 
-//                 + + + + + + + + + + + 
-//               + + + + + + + + + + + 
-//             + + + + + + + + + + + 
-//           + + + + + + + + + + + 
-//         + + + + + + + + + + + 
-//       + + + + + + + + + + + 
-//     + + + + + + + + + + + 
-//   + + + + + + + + + + + 
-
-// public class starPattern {
-//     public static void main(String agr[]) {
-//         for(int i=10; i>=0;i--){
-//             for(int j=0;j<=i;j++){
-//                 System.out.print("  ");
-//             }
-//             for(int k=0;k<=10;k++){
-//                 System.out.print("+ ");
-//             }
-//             System.out.print("\n");
-//         }
-//     }
-// }
-
-
-// OUTPUT 
-
-//                        +  +  +  +  +  +  +  +  +  +  + 
-//                      +                             + 
-//                    +                             + 
-//                  +                             + 
-//                +                             + 
-//              +                             + 
-//            +                             + 
-//          +                             + 
-//        +                             + 
-//      +                             + 
-//    +  +  +  +  +  +  +  +  +  +  + 
-
-
-// public class starPattern {
-//     public static void main(String agr[]) {
-//         for(int i=10; i>=0;i--){
-//             for(int j=0;j<=i;j++){
-//                 System.out.print("  ");
-//             }
-//             for(int k=0;k<=10;k++){
-//                 if(k==0 || k==10 || i==0 || i==10){
-//                     System.out.print(" + ");
-//                 } else{
-//                     System.out.print("   ");
-//                 }
-//             }
-//             System.out.print("\n");
-//         }
-//     }
-// }
-
-
-// OUTPUT 
-//            *
-//           ***
-//          *****
-//         *******
-//        *********
-//       ***********
-//      *************
-//     ***************
-//    *****************
-//   *******************
-//  *********************
-
-// public class starPattern {
-//     public static void main(String agr[]) {
-//         for(int i=10;i>=0;i--){
-//             for(int j=0 ; j<=i;j++){
-//                 System.out.print(" ");
-//             }
-//             for(int k = i; k<=10;k++){
-//                 System.out.print("*");
-//             }
-//             for(int x = i+1; x<=10;x++){
-//                 System.out.print("*");
-//             }
-//             System.out.print("\n");
-//         }
-//     }
-// }
-
 public class starPattern {
-    public static void main(String[] args) {
-        int rows = 10;
-
-        for (int i = 0; i < rows; i++) {
-
-            // Spaces
-            for (int j = 0; j < rows - i - 1; j++) {
-                System.out.print(" ");
+    public static void main(String agr[]) {
+        for (int i = 1; i <= 5; i++) {
+            if (i % 2 == 0) {
+                for (int j = 1; j <= i; j++) {
+                    if (j % 2 == 0) {
+                        System.out.print("1");
+                    } else {
+                        System.out.print("0");
+                    }
+                }
+            } else {
+                for (int j = 1; j <= i; j++) {
+                    if (j % 2 == 0) {
+                        System.out.print("0");
+                    } else {
+                        System.out.print("1");
+                    }
+                }
             }
-
-            // Ones
-            for (int j = 0; j < 2 * i + 1; j++) {
-                System.out.print("1");
-            }
-
-            System.out.println();
+            System.out.print("\n");
         }
     }
 }
+
+// OUTPUT
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+// 11 12 13 14 15
+// 16 17 18 19 20 21
+// 22 23 24 25 26 27 28
+// 29 30 31 32 33 34 35 36
+// 37 38 39 40 41 42 43 44 45
+// 46 47 48 49 50 51 52 53 54 55
+// 56 57 58 59 60 61 62 63 64 65 66
+
+// public class starPattern {
+// public static void main(String agr[]) {
+// int num = 1;
+// for (int i = 0; i <= 10; i++) {
+// for(int j =0; j<=i; j++){
+// System.out.print(num+ " ");
+// num++;
+// }
+// System.out.print("\n");
+// }
+// }
+// }
+
+// OUTPUT
+// + + + + + + + + + + +
+// + + + + + + + + + + +
+// + + + + + + + + + + +
+// + + + + + + + + + + +
+// + + + + + + + + + + +
+// + + + + + + + + + + +
+// + + + + + + + + + + +
+// + + + + + + + + + + +
+// + + + + + + + + + + +
+// + + + + + + + + + + +
+// + + + + + + + + + + +
+
+// public class starPattern {
+// public static void main(String agr[]) {
+// for(int i=10; i>=0;i--){
+// for(int j=0;j<=i;j++){
+// System.out.print(" ");
+// }
+// for(int k=0;k<=10;k++){
+// System.out.print("+ ");
+// }
+// System.out.print("\n");
+// }
+// }
+// }
+
+// OUTPUT
+
+// + + + + + + + + + + +
+// + +
+// + +
+// + +
+// + +
+// + +
+// + +
+// + +
+// + +
+// + +
+// + + + + + + + + + + +
+
+// public class starPattern {
+// public static void main(String agr[]) {
+// for(int i=10; i>=0;i--){
+// for(int j=0;j<=i;j++){
+// System.out.print(" ");
+// }
+// for(int k=0;k<=10;k++){
+// if(k==0 || k==10 || i==0 || i==10){
+// System.out.print(" + ");
+// } else{
+// System.out.print(" ");
+// }
+// }
+// System.out.print("\n");
+// }
+// }
+// }
+
+// OUTPUT
+// *
+// ***
+// *****
+// *******
+// *********
+// ***********
+// *************
+// ***************
+// *****************
+// *******************
+// *********************
+
+// public class starPattern {
+// public static void main(String agr[]) {
+// for(int i=10;i>=0;i--){
+// for(int j=0 ; j<=i;j++){
+// System.out.print(" ");
+// }
+// for(int k = i; k<=10;k++){
+// System.out.print("*");
+// }
+// for(int x = i+1; x<=10;x++){
+// System.out.print("*");
+// }
+// System.out.print("\n");
+// }
+// }
+// }
+
+// public class starPattern {
+// public static void main(String[] args) {
+// int rows = 10;
+
+// for (int i = 0; i < rows; i++) {
+
+// // Spaces
+// for (int j = 0; j < rows - i - 1; j++) {
+// System.out.print(" ");
+// }
+
+// // Ones
+// for (int j = 0; j < 2 * i + 1; j++) {
+// System.out.print("1");
+// }
+
+// System.out.println();
+// }
+// }
+// }
