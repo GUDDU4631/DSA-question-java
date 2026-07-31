79979ecfc0f4333b9c2d8d9e9a96e19592a0df6a
GUDDU4631 <guddukumarchaudhary3547@gmail.com>
2026-07-31 09:14:35 +0530

    day-9

---
+++ Leetcode/reverseNum7.java
@@ -0,0 +1,31 @@
+class reverseNum {
+    public int reverse(int x) {
+       int newNum =0 ;
+       int neg =0;
+       if(x < 0){
+        neg = x;
+        x = -x ;
+       }
+       while (x > 0){
+        int r = x % 10;
+        x = x / 10;
+        if (newNum > Integer.MAX_VALUE / 10 ||
+            (newNum == Integer.MAX_VALUE / 10 && r > 7)) {
+            return 0;
+        }
+
+        // Check for underflow
+        if (newNum < Integer.MIN_VALUE / 10 ||
+            (newNum == Integer.MIN_VALUE / 10 && r < -8)) {
+            return 0;
+        }
+        newNum = (newNum*10) + r;
+        
+       } 
+       if(neg < 0){
+        newNum = -newNum;
+       }
+       
+       return newNum;
+    }
+}
