b38f3dd0bfff4f8d06e439952b275dec1f2a9df4
GUDDU4631 <guddukumarchaudhary3547@gmail.com>
2026-08-08 21:24:19 +0530

    Day-16

---
+++ Leetcode/SetMatrixZeroes73.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+
+public class SetMatrixZeroes73 {
+    public void setZeroes(int[][] matrix) {
+        HashSet<Integer> zeroRows = new HashSet<>();
+        HashSet<Integer> zeroCols = new HashSet<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+
+                if (matrix[i][j] == 0) {
+                    zeroRows.add(i);
+                    zeroCols.add(j);
+                }
+            }
+        }
+        for (int value : zeroRows) {
+            for(int i=0;i<matrix[0].length;i++){
+                matrix[value][i]=0;
+            }
+        }
+        for (int value : zeroCols) {
+            for(int i=0;i<matrix.length;i++){
+                matrix[i][value]=0;
+            }
+        }
+    }
+}
