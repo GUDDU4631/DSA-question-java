8f9544902d9a1f4553c023220225b232c5007255
Guddu Chaudhary <121762737+GUDDU4631@users.noreply.github.com>
2026-08-20 08:17:21 +0530

    Time: 1 ms (99.94%) | Memory: 45.3 MB (81.31%) - LeetSync

---
+++ 443-string-compression/string-compression.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int compress(char[] chars) {
+
+        int write = 0;
+        for (int i = 0; i < chars.length; i++) {
+            int count = 1;
+            while (i < chars.length - 1 && chars[i] == chars[i + 1]) {
+                count++;
+                i++;
+            }
+            chars[write++] = chars[i];
+            if (count > 1) {
+                String countString = String.valueOf(count);
+                for(int j=0;j<countString.length();j++){
+                    chars[write++] = countString.charAt(j);
+                }
+            }
+        }
+        return write;
+    }
+}
